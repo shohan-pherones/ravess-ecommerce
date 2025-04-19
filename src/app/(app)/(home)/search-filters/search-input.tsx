7cc571648c18678be75825121f7bafc508c0c142
shohan-pherones <shohan.sub.56@gmail.com>
2025-04-20 00:19:45 +0600

    add categories finalization

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,13 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { useState } from "react";
+import { CategoriesSidebar } from "./categories-sidebar";
+import { CustomCategory } from "./types";
 
 interface Props {
   disabled?: boolean;
+  data: CustomCategory[];
 }
 
-export const SearchInput = ({ disabled }: Props) => {
+export const SearchInput = ({ disabled, data }: Props) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex items-center gap-2 w-full">
+      <CategoriesSidebar
+        data={data}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
       <div className="relative w-full">
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
         <Input
@@ -16,7 +30,13 @@ export const SearchInput = ({ disabled }: Props) => {
           disabled={disabled}
         />
       </div>
-      {/* TODO: add categories view all button */}
+      <Button
+        variant="elevated"
+        className="size-12 shrink-0 flex lg:hidden"
+        onClick={() => setIsSidebarOpen(true)}
+      >
+        <ListFilterIcon />
+      </Button>
       {/* TODO: add library button */}
     </div>
   );

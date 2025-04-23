8b471b1f355cd615d7861827acde1937c4550f4d
shohan-pherones <shohan.sub.56@gmail.com>
2025-04-23 14:48:19 +0600

    fix: tackle favicon.ico from being passed for prefetching products

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -43,11 +43,11 @@ export const productsRouter = createTRPCRouter({
               (subcategory) => subcategory.slug
             )
           );
-        }
 
-        where["category.slug"] = {
-          in: [parentCategory.slug, ...subcategoriesSlugs],
-        };
+          where["category.slug"] = {
+            in: [parentCategory.slug, ...subcategoriesSlugs],
+          };
+        }
       }
 
       const data = await ctx.db.find({
